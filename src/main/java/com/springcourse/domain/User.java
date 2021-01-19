bd14dcf14210a5619b10d510cc7812e156cbe85c
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-01-19 17:31:28 -0300

    Create entities

---
+++ src/main/java/com/springcourse/domain/User.java
@@ -0,0 +1,22 @@
+package com.springcourse.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private List<Request> requests = new ArrayList<Request>();
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
+}
