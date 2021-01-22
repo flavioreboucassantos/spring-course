3d59b32343af830fd0000bc0c8ed6bddc50c9ea1
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-01-22 17:02:09 -0300

    Create UserLoginDTO.java

---
+++ src/main/java/com/springcourse/dto/UserLoginDTO.java
@@ -0,0 +1,16 @@
+package com.springcourse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserLoginDTO {
+	private String email;
+	private String password;
+
+}
