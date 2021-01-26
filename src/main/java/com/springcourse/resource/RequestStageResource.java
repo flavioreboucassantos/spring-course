25fac4c446241d4200aaae2e58e7684ab23264c5
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-01-26 17:19:43 -0300

    Input validation

--- src/main/java/com/springcourse/resource/RequestStageResource.java
+++ src/main/java/com/springcourse/resource/RequestStageResource.java
@@ -1,5 +1,7 @@
 package com.springcourse.resource;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.domain.RequestStage;
+import com.springcourse.dto.RequestStageSaveDTO;
 import com.springcourse.service.RequestStageService;
 
 @RestController
@@ -20,7 +23,8 @@ public class RequestStageResource {
 	private RequestStageService requestStageService;
 
 	@PostMapping
-	public ResponseEntity<RequestStage> save(@RequestBody RequestStage requestStage) {
+	public ResponseEntity<RequestStage> save(@RequestBody @Valid RequestStageSaveDTO requestStageSaveDTO) {
+		RequestStage requestStage = requestStageSaveDTO.transformToRequestStage();
 		RequestStage createdRequestStage = requestStageService.save(requestStage);
 		return ResponseEntity.status(HttpStatus.CREATED).body(createdRequestStage);
 	}
