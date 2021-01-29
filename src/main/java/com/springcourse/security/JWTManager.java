9e3a15702cf666784f60aceda689046049a50896
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-01-29 18:06:55 -0300

    Add parse token method

--- src/main/java/com/springcourse/security/JWTManager.java
+++ src/main/java/com/springcourse/security/JWTManager.java
@@ -8,24 +8,35 @@ import org.springframework.stereotype.Service;
 import com.springcourse.constant.SecurityConstants;
 import com.springcourse.dto.UserLoginResponseDTO;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
-public class JWTManager {
+public class JwtManager {
 	public UserLoginResponseDTO createToken(String email, List<String> roles) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.add(Calendar.DAY_OF_MONTH, SecurityConstants.JWT_EXP_DAYS);
-		
+
 		String jwt = Jwts.builder()
 				.setSubject(email)
 				.setExpiration(calendar.getTime())
 				.claim(SecurityConstants.JWT_ROLE_KEY, roles)
 				.signWith(SignatureAlgorithm.HS512, SecurityConstants.API_KEY.getBytes())
 				.compact();
-		
+
 		Long expireIn = calendar.getTimeInMillis();
-		
+
 		return new UserLoginResponseDTO(jwt, expireIn, SecurityConstants.JWT_PROVIDER);
+	}
+
+	public Claims parseToken(String jwt) throws JwtException {
+		Claims claims = Jwts.parser()
+				.setSigningKey(SecurityConstants.API_KEY.getBytes())
+				.parseClaimsJwt(jwt)
+				.getBody();
+
+		return claims;
 	}
 }
