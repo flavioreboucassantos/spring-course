e348f4b6789ef0660d688db162e6b2ce6d1567a4
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-01-30 17:49:41 -0300

    New API_KEY

--- src/main/java/com/springcourse/constant/SecurityConstants.java
+++ src/main/java/com/springcourse/constant/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.springcourse.constant;
 
 public class SecurityConstants {
 	public static final int JWT_EXP_DAYS = 5;
-	public static final String API_KEY = "spring-course";
+	public static final String API_KEY = "a64PZGhUzxYmXz4j";
 	public static final String JWT_PROVIDER = "Bearer";
 	public static final String JWT_ROLE_KEY = "role";
 	public static final String JWT_INVALID_MSG = "Invalid Jwt token";
