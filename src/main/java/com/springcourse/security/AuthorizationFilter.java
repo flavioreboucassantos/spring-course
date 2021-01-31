110a8ff5d6f1ba25b1112bc6bc821798f355e56c
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-01-31 17:51:03 -0300

    Limpeza de imports

--- src/main/java/com/springcourse/security/AuthorizationFilter.java
+++ src/main/java/com/springcourse/security/AuthorizationFilter.java
@@ -11,7 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
